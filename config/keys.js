39294786a45426ca2ef46a57e01c779552f7329e
Kritagya61 <kritagya61@gmail.com>
2020-10-15 14:26:08 +0530

    second commit

--- config/keys.js
+++ config/keys.js
@@ -1,13 +1,14 @@
 //add this file to gitignore
 module.exports = {
-    google: {
-        clientID: '662863904898-vv6lt5ubfmh7t2151mhifjob2fkbmler.apps.googleusercontent.com',
-        clientSecret: 'T0hpwDqPt2yrNwxsqZqB_yaP'
-    },
-    mongodb: {
-        dbURI: 'mongodb+srv://netninja:operation32@cluster0-0bemi.mongodb.net/test'
-    },
-    session: {
-        cookieKey: 'thenetninjaisawesomeiguess'
-    }
+  google: {
+    clientID:
+      "662863904898-vv6lt5ubfmh7t2151mhifjob2fkbmler.apps.googleusercontent.com",
+    clientSecret: "T0hpwDqPt2yrNwxsqZqB_yaP",
+  },
+  mongodb: {
+    dbURI: "",
+  },
+  session: {
+    cookieKey: "thenetninjaisawesomeiguess",
+  },
 };
