07e9d3de77938f129166350e7bc9097834c92eb2
Kritagya61 <kritagya61@gmail.com>
2020-10-15 14:17:42 +0530

    initial commit

---
+++ config/keys.js
@@ -0,0 +1,13 @@
+//add this file to gitignore
+module.exports = {
+    google: {
+        clientID: '662863904898-vv6lt5ubfmh7t2151mhifjob2fkbmler.apps.googleusercontent.com',
+        clientSecret: 'T0hpwDqPt2yrNwxsqZqB_yaP'
+    },
+    mongodb: {
+        dbURI: 'mongodb+srv://netninja:operation32@cluster0-0bemi.mongodb.net/test'
+    },
+    session: {
+        cookieKey: 'thenetninjaisawesomeiguess'
+    }
+};
